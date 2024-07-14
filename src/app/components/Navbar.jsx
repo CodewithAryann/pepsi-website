6a9623d051b2979902030b4e442e39bea2716c0d
Aryan <151847516+CodewithAryann@users.noreply.github.com>
2024-07-14 17:13:48 +0400

    7/14/24

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { IoIosArrowDown } from "react-icons/io";
 
 function nav() {
   return (
@@ -6,7 +7,7 @@ function nav() {
     <nav className='flex justify-between p-[12px] pr-7 pl-7 bg-[#0025ff] items-center font-Poppins font-black uppercase text-sm'>
       <ul className='flex items-center space-x-20'>
         <li><img className='nav-img w-16' src="https://www.pepsi.com/en-us/refresh082123/images/PepsiLogo@3x.png" alt="" /></li>
-        <li><a href="">View Products</a></li>
+        <li className='products flex gap-2 items-center'><a  href="">View Products</a> <span className='font-bolder text-[15px]'><IoIosArrowDown /></span></li>
         <li><a href="">Contact Us</a></li>
       </ul>
       <ul className='flex ml-auto'>
