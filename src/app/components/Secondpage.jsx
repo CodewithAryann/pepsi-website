41b6363b83c52c95a4dbe07851996770b51eddef
Aryan <151847516+CodewithAryann@users.noreply.github.com>
2024-07-14 18:09:44 +0400

    7/14/24

--- src/app/components/Secondpage.jsx
+++ src/app/components/Secondpage.jsx
@@ -7,7 +7,7 @@ function Secondpage() {
         backgroundImage: `url('https://www.pepsi.com/en-us/refresh082123/images/wild-cherry/3296846-wild-cherry-2024-digital-rd-3-1920-x-1080@2x.png')`,
         backgroundSize: 'cover',
       }} className='w-full h-[75vw]' >
-        <button class="btn-12"><span>Button</span></button>
+        <button class="btn-12 relative top-[64vw] left-[35vw]"><span>Shop Now</span></button>
 
       </div>
     </div>
