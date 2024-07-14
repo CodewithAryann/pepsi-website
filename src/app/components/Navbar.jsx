2658802bcf6fae2c20011ddfbd01a585658698e7
Aryan <151847516+CodewithAryann@users.noreply.github.com>
2024-07-14 15:13:19 +0400

    7/14/24

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 function nav() {
   return (
     <div>
-    <nav className='flex justify-between p-[20px] bg-blue-700 items-center font-Poppins font-extrabold'>
+    <nav className='flex justify-between p-[20px] bg-blue-700 items-center font-Poppins font-extrabold uppercase text-sm'>
       <ul className='flex items-center space-x-20'>
         <li><img className='w-16' src="https://www.pepsi.com/en-us/refresh082123/images/PepsiLogo@3x.png" alt="" /></li>
         <li><a href="">View Products</a></li>
         <li><a href="">Contact Us</a></li>
       </ul>
       <ul className='flex ml-auto'>
-        <li><button><span>Buy Pepsi Products</span></button></li>
+        <li><button><span className='uppercase'>Buy Pepsi Products</span></button></li>
       </ul>
     </nav>
   </div>
