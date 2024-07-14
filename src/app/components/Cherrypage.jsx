23a0bc89fe69fe352a6b1be7035c98c3f309f471
Aryan <151847516+CodewithAryann@users.noreply.github.com>
2024-07-14 18:28:16 +0400

    7/14/24

--- src/app/components/Cherrypage.jsx
+++ src/app/components/Cherrypage.jsx
@@ -2,12 +2,23 @@ import React from 'react'
 
 function Cherrypage() {
   return (
-    <div>
        <div  style={{
         backgroundImage: `url('https://www.pepsi.com/en-us/refresh082123/images/wild-cherry/wc-tvc-bg-tablet-vert@2x.png')`,
         backgroundSize: 'cover',
-      }} className='w-full h-[75vw]' ></div>
-    </div>
+      }} className='w-full h-[75vw]' >
+        <img className='mt-10' src="https://www.pepsi.com/en-us/refresh082123/images/wild-cherry/get-wild-with-pepsi-wild-cherry-tab-vert@2x.png" alt="" />
+        <div className='cards flex p-10 gap-6'>
+            <div className='grid justify-items-center'>
+                <img src="https://www.pepsi.com/en-us/refresh082123/images/wild-cherry/231214-07-pepsi-1094-v-4-16-x-9-tab-horz@2x.png" alt="" />
+                <button className='mt-10 bg-white w-[31vw] h-[5.3vw] font-[2.8vw] text-black'><a href="">#1 Fan</a></button>
+            </div>
+            <div className='grid justify-items-center'>
+                <img src="https://www.pepsi.com/en-us/refresh082123/images/wild-cherry/15-20-52-13-v-4-16-x-9-tab-horz@2x.png" alt="" />
+                <button className='mt-10 bg-white w-[31vw] h-[5.3vw] font-[2.8vw] text-black'><a href="">Nursery Rhymez</a></button>
+            </div>
+        </div>
+      </div>
+
   )
 }
 
--- src/app/components/Secondpage.jsx
+++ src/app/components/Secondpage.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 function Secondpage() {
   return (
-    <div>
       <div  style={{
         backgroundImage: `url('https://www.pepsi.com/en-us/refresh082123/images/wild-cherry/3296846-wild-cherry-2024-digital-rd-3-1920-x-1080@2x.png')`,
         backgroundSize: 'cover',
@@ -10,7 +9,6 @@ function Secondpage() {
         <button class="btn-12 relative top-[64vw] left-[35vw]"><span>Shop Now</span></button>
 
       </div>
-    </div>
   )
 }
 
