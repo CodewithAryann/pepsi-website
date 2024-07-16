06e2d5b937fb8f76baacf0d97538233614ebc1cd
Aryan <151847516+CodewithAryann@users.noreply.github.com>
2024-07-16 17:53:40 +0400

    7/16/24

--- src/app/components/Stayconnected.jsx
+++ src/app/components/Stayconnected.jsx
@@ -1,23 +1,139 @@
-import React from 'react'
+import React from "react";
 
 function Stayconnected() {
   return (
     <div>
-      <h1 className='font-[ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont,
+      <h1
+        className="font-[ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont,
     Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif,
-    Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji] uppercase font-black text-center text-5xl mt-10'>STAY ON THE PULSE. FOLLOW PEPSI.</h1>
-    <div className='social'>
-        <div>
-            <a href="https://www.instagram.com/pepsi" target="_blank" data-gtm-category="Social" data-gtm-action="CTA" aria-label="Instagram, opens a in new tab" data-gtm-label="Instagram"><img class="social__item-image" src="https://www.pepsi.com/en-us/refresh082123/images/social/PEPSI_2023_PR_WCPZS_Cans.png" alt="Follow Us On Instagram" role="presentation" loading="lazy" width="auto" height="100%"></img></a>
-            {/* <a href=""><img class="social__item-image" src="https://www.pepsi.com/en-us/refresh082123/images/social/PEPSI_2023_PR_WCPZS_Cans.png" alt="Follow Us On Instagram" role="presentation" loading="lazy" width="auto" height="100%"></img></a> */}
-            
+    Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji] uppercase font-black text-center text-5xl mt-10"
+      >
+        STAY ON THE PULSE. FOLLOW PEPSI.
+      </h1>
+      <div className="social flex border-solid border-blue-700 border-2 w-[90%] h-auto m-auto mt-10">
+        <div className="social-item instagram-link relative">
+          <a
+            href="https://www.instagram.com/pepsi"
+            target="_blank"
+            data-gtm-category="Social"
+            data-gtm-action="CTA"
+            aria-label="Instagram, opens a in new tab"
+            data-gtm-label="Instagram"
+          >
+            <img
+              className="social__item-image instagram-icon"
+              src="https://www.pepsi.com/en-us/refresh082123/images/social/PEPSI_2023_PR_WCPZS_Cans.png"
+              alt="Follow Us On Instagram"
+              role="presentation"
+              loading="lazy"
+              width="auto"
+              height="100%"
+            />
+          </a>
         </div>
-        <div><a href="https://twitter.com/pepsi" target="_blank" data-gtm-category="Social" data-gtm-action="CTA" aria-label="Twitter, opens a in new tab" data-gtm-label="Twitter"><img class="social__item-image" src="https://www.pepsi.com/en-us/refresh082123/images/social/PEPSI_2023_PR_Licensing.png" alt="Follow Us On Twitter" role="presentation" loading="lazy" width="auto" height="100%"></img></a></div>
-        <div><a href="https://www.facebook.com/PepsiUS" target="_blank" data-gtm-category="Social" data-gtm-action="CTA" aria-label="Facebook, opens a in new tab" data-gtm-label="Facebook"><img class="social__item-image" src="https://www.pepsi.com/en-us/refresh082123/images/social/PEPSI_2023_PR_Logo+Cans.png" alt="Check us out on Facebook" role="presentation" loading="lazy" width="auto" height="100%"></img></a></div>
-        <div><a href="https://www.youtube.com/user/Pepsi" target="_blank" data-gtm-category="Social" data-gtm-action="CTA" aria-label="Youtube, opens a in new tab" data-gtm-label="Youtube"><img class="social__item-image" src="https://www.pepsi.com/en-us/refresh082123/images/social/PEP_TITAN_R34_LaunchAssets_Burger_EF_v1.png" alt="Check us out on Youtube" role="presentation" loading="lazy" width="auto" height="100%"></img></a></div>
-        <div><a href="https://www.tiktok.com/@pepsi" target="_blank" data-gtm-category="Social" data-gtm-action="CTA" aria-label="TikTok, opens a in new tab" data-gtm-label="TikTok"><img class="social__item-image" src="https://www.pepsi.com/en-us/refresh082123/images/social/PEPSI_2023_PR_Photography_Overhead.png" alt="Check us out on TikTok" role="presentation" loading="lazy" width="auto" height="100%"></img></a></div><a href="https://www.threads.net/@pepsi" target="_blank" data-gtm-category="Social" data-gtm-action="CTA" aria-label="Threads, opens a in new tab" data-gtm-label="Threads"><img class="social__item-image" src="https://www.pepsi.com/en-us/refresh082123/images/social/THREADS.png" alt="Check us out on TikTok" role="presentation" loading="lazy" width="auto" height="100%"></img></a></div>
+        <div className="social-item x-link relative">
+          <a
+            href="https://twitter.com/pepsi"
+            target="_blank"
+            data-gtm-category="Social"
+            data-gtm-action="CTA"
+            aria-label="Instagram, opens a in new tab"
+            data-gtm-label="Instagram"
+          >
+            <img
+              className="social__item-image twitter-icon"
+              src="https://www.pepsi.com/en-us/refresh082123/images/social/PEPSI_2023_PR_Licensing.png"
+              alt="Follow Us On Twitter"
+              role="presentation"
+              loading="lazy"
+              width="auto"
+              height="100%"
+            />
+          </a>
+        </div>
+        <div className="social-item facebook-link relative">
+          <a
+            href="https://www.facebook.com/PepsiUS"
+            target="_blank"
+            data-gtm-category="Social"
+            data-gtm-action="CTA"
+            aria-label="Facebook, opens a in new tab"
+            data-gtm-label="Facebook"
+          >
+            <img
+              className="social__item-image facebook-icon"
+              src="https://www.pepsi.com/en-us/refresh082123/images/social/PEPSI_2023_PR_Logo+Cans.png"
+              alt="Check us out on Facebook"
+              role="presentation"
+              loading="lazy"
+              width="auto"
+              height="100%"
+            />
+          </a>
+        </div>
+        <div className="social-item relative">
+          <a
+            href="https://www.youtube.com/user/Pepsi"
+            target="_blank"
+            data-gtm-category="Social"
+            data-gtm-action="CTA"
+            aria-label="Youtube, opens a in new tab"
+            data-gtm-label="Youtube"
+          >
+            <img
+              className="social__item-image youtube-icon"
+              src="https://www.pepsi.com/en-us/refresh082123/images/social/PEP_TITAN_R34_LaunchAssets_Burger_EF_v1.png"
+              alt="Check us out on Youtube"
+              role="presentation"
+              loading="lazy"
+              width="auto"
+              height="100%"
+            />
+          </a>
+        </div>
+        <div className="social-item relative">
+          <a
+            href="https://www.tiktok.com/@pepsi"
+            target="_blank"
+            data-gtm-category="Social"
+            data-gtm-action="CTA"
+            aria-label="TikTok, opens a in new tab"
+            data-gtm-label="TikTok"
+          >
+            <img
+              className="social__item-image tiktok-icon"
+              src="https://www.pepsi.com/en-us/refresh082123/images/social/PEPSI_2023_PR_Photography_Overhead.png"
+              alt="Check us out on TikTok"
+              role="presentation"
+              loading="lazy"
+              width="auto"
+              height="100%"
+            />
+          </a>
+        </div>
+        <div className="social-item relative">
+          <a
+            href="https://www.threads.net/@pepsi"
+            target="_blank"
+            data-gtm-category="Social"
+            data-gtm-action="CTA"
+            aria-label="Threads, opens a in new tab"
+            data-gtm-label="Threads"
+          >
+            <img
+              className="social__item-image threads-icon"
+              src="https://www.pepsi.com/en-us/refresh082123/images/social/THREADS.png"
+              alt="Check us out on Threads"
+              role="presentation"
+              loading="lazy"
+              width="auto"
+              height="100%"
+            />
+          </a>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Stayconnected
+export default Stayconnected;
