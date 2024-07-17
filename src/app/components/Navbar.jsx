de72a22dd51034ef8727e3d56aeadb33b1eb9cc4
Aryan <151847516+CodewithAryann@users.noreply.github.com>
2024-07-17 17:14:12 +0400

    7/14/24

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -37,61 +37,111 @@ function Nav() {
       </nav>
       <div 
         ref={productsRef} 
-        className={` top-0 left-0 w-full h-full bg-[#0025ff] text-white flex flex-col justify-center items-center z-50 ${showProducts ? 'flex' : 'hidden'}`}
+        className={`top-0 left-0 w-full bg-[#0025ff] text-white flex flex-col justify-center items-center z-50 ${showProducts ? 'flex' : 'hidden'}`}
+        style={{ position: 'fixed', overflowY: 'auto', top: '0', bottom: '0', left: '0', right: '0', scrollbarWidth: 'none', paddingRight: '15px'}}
       >
         <button onClick={toggleProducts} className='absolute top-4 right-4 text-white font-bold'>Close</button>
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-8 p-8">
-          <div className="text-center">
-            <img src="https://www.pepsi.com/en-us/refresh082123/images/cans/44341_Titan_Pep_Can_12oz_FR.png" alt="Pepsi" />
-            <p className="mt-2 font-bold">Pepsi</p>
+        <h1 className="absolute top-8 text-white text-3xl font-bold">Pepsi Products</h1>
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-8 p-8 w-full mt-[42vw]">
+          <div className="hover:bg-gray-200 rounded-lg overflow-hidden shadow-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 h-[19vw]">
+            <p className="mt-2 text-center font-bold">Pepsi</p>
+            <div className="relative overflow-hidden h-[23vw] top-[11%] left-[24%]">
+              <img
+                src="https://www.pepsi.com/en-us/refresh082123/images/cans/44341_Titan_Pep_Can_12oz_FR.png"
+                alt="Pepsi"
+                className="absolute inset-0 h-full object-cover w-[50%]"
+              />
+            </div>
           </div>
-          <div className="text-center">
-            <img src="https://www.pepsi.com/en-us/refresh082123/images/cans/44343_Titan_PZ_Can_12oz_FR.png" alt="Pepsi Zero Sugar" />
-            <p className="mt-2 font-bold">Pepsi Zero Sugar</p>
+          <div className="hover:bg-gray-200 rounded-lg overflow-hidden shadow-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 h-[19vw]">
+            <p className="mt-2 text-center font-bold">Pepsi Zero Sugar</p>
+            <div className="relative overflow-hidden h-[23vw] top-[11%] left-[24%]">
+              <img
+                src="https://www.pepsi.com/en-us/refresh082123/images/cans/44343_Titan_PZ_Can_12oz_FR.png"
+                alt="Pepsi Zero Sugar"
+                className="absolute inset-0 h-full object-cover w-[50%]"
+              />
+            </div>
           </div>
-          <div className="text-center">
-            <img src="https://www.pepsi.com/en-us/refresh082123/images/cans/44344_EC_Front.png" alt="Diet Pepsi" />
-            <p className="mt-2 font-bold">Diet Pepsi</p>
+          <div className="hover:bg-gray-200 rounded-lg overflow-hidden shadow-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 h-[19vw]">
+            <p className="mt-2 text-center font-bold">Diet Pepsi</p>
+            <div className="relative overflow-hidden h-[23vw] top-[11%] left-[24%]">
+              <img
+                src="https://www.pepsi.com/en-us/refresh082123/images/cans/44344_EC_Front.png"
+                alt="Diet Pepsi"
+                className="absolute inset-0 h-full object-cover w-[50%]"
+              />
+            </div>
           </div>
-          <div className="text-center">
-            <img src="https://www.pepsi.com/en-us/refresh082123/images/cans/Nitro_Can_for_4pk_Cart_FD_Vanilla.png" alt="Nitro Pepsi Vanilla" />
-            <p className="mt-2 font-bold">Nitro Pepsi Vanilla</p>
+          <div className="hover:bg-gray-200 rounded-lg overflow-hidden shadow-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 h-[19vw]">
+            <p className="mt-2 text-center font-bold">Nitro Pepsi Vanilla</p>
+            <div className="relative overflow-hidden h-[23vw] top-[11%] left-[24%]">
+              <img
+                src="https://www.pepsi.com/en-us/refresh082123/images/cans/Nitro_Can_for_4pk_Cart_FD_Vanilla.png"
+                alt="Nitro Pepsi Vanilla"
+                className="absolute inset-0 h-full object-cover w-[50%]"
+              />
+            </div>
           </div>
-          <div className="text-center">
-            <img src="https://www.pepsi.com/en-us/refresh082123/images/cans/Nitro_Can_for_4pk_Cart_FD.png" alt="Nitro Pepsi" />
-            <p className="mt-2 font-bold">Nitro Pepsi</p>
+          <div className="hover:bg-gray-200 rounded-lg overflow-hidden shadow-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 h-[19vw]">
+            <p className="mt-2 text-center font-bold">Nitro Pepsi</p>
+            <div className="relative overflow-hidden h-[23vw] top-[11%] left-[24%]">
+              <img
+                src="https://www.pepsi.com/en-us/refresh082123/images/cans/Nitro_Can_for_4pk_Cart_FD.png"
+                alt="Nitro Pepsi"
+                className="absolute inset-0 h-full object-cover w-[50%]"
+              />
+            </div>
           </div>
-          <div className="text-center">
-            <img src="https://www.pepsi.com/en-us/refresh082123/images/cans/44342_Titan_PMG_Can_12oz_FR.png" alt="Pepsi Mango" />
-            <p className="mt-2 font-bold">Pepsi Mango</p>
+          <div className="hover:bg-gray-200 rounded-lg overflow-hidden shadow-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 h-[19vw]">
+            <p className="mt-2 text-center font-bold">Pepsi Mango</p>
+            <div className="relative overflow-hidden h-[23vw] top-[11%] left-[24%]">
+              <img
+                src="https://www.pepsi.com/en-us/refresh082123/images/cans/44342_Titan_PMG_Can_12oz_FR.png"
+                alt="Pepsi Mango"
+                className="absolute inset-0 h-full object-cover w-[50%]"
+              />
+            </div>
           </div>
-          <div className="text-center">
-            <img src="https://www.pepsi.com/en-us/refresh082123/images/cans/44344_EC_Front_2.png" alt="Pepsi Zero Sugar Mango" />
-            <p className="mt-2 font-bold">Pepsi Zero Sugar Mango</p>
+          <div className="hover:bg-gray-200 rounded-lg overflow-hidden shadow-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 h-[19vw]">
+            <p className="mt-2 text-center font-bold">Pepsi Zero Sugar Mango</p>
+            <div className="relative overflow-hidden h-[23vw] top-[11%] left-[24%]">
+              <img
+                src="https://www.pepsi.com/en-us/refresh082123/images/cans/44344_EC_Front_2.png"
+                alt="Pepsi Zero Sugar Mango"
+                className="absolute inset-0 h-full object-cover w-[50%]"
+              />
+            </div>
           </div>
-          <div className="text-center">
-            <img src="https://www.pepsi.com/en-us/refresh082123/images/cans/44347_Titan_PWC_Can_12oz_FR.png" alt="Pepsi Wild Cherry" />
-            <p className="mt-2 font-bold">Pepsi Wild Cherry</p>
+          <div className="hover:bg-gray-200 rounded-lg overflow-hidden shadow-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 h-[19vw]">
+            <p className="mt-2 text-center font-bold">Pepsi Wild Cherry</p>
+            <div className="relative overflow-hidden h-[23vw] top-[11%] left-[24%]">
+              <img
+                src="https://www.pepsi.com/en-us/refresh082123/images/cans/44347_Titan_PWC_Can_12oz_FR.png"
+                alt="Pepsi Wild Cherry"
+                className="absolute inset-0 h-full object-cover w-[50%]"
+              />
+            </div>
           </div>
-          <div className="text-center">
-            <img src="https://www.pepsi.com/en-us/refresh082123/images/cans/44340_EC_Front.png" alt="Diet Pepsi Wild Cherry" />
-            <p className="mt-2 font-bold">Diet Pepsi Wild Cherry</p>
+          <div className="hover:bg-gray-200 rounded-lg overflow-hidden shadow-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 h-[19vw]">
+            <p className="mt-2 text-center font-bold">Diet Pepsi Wild Cherry</p>
+            <div className="relative overflow-hidden h-[23vw] top-[11%] left-[24%]">
+              <img
+                src="https://www.pepsi.com/en-us/refresh082123/images/cans/44340_EC_Front.png"
+                alt="Diet Pepsi Wild Cherry"
+                className="absolute inset-0 h-full object-cover w-[50%]"
+              />
+            </div>
           </div>
-          <div className="text-center">
-            <img src="https://www.pepsi.com/en-us/refresh082123/images/cans/44346_Titan_PZWC_Can_12oz_FR.png" alt="Pepsi Zero Sugar Wild Cherry" />
-            <p className="mt-2 font-bold">Pepsi Zero Sugar Wild Cherry</p>
-          </div>
-          <div className="text-center">
-            <img src="https://www.pepsi.com/en-us/refresh082123/images/cans/44337_Titan_CFP_Can_12oz_FR.png" alt="Pepsi Caffeine Free" />
-            <p className="mt-2 font-bold">Pepsi Caffeine Free</p>
-          </div>
-          <div className="text-center">
-            <img src="https://www.pepsi.com/en-us/refresh082123/images/cans/44336_Titan_CFDP_Can_12oz_FR.png" alt="Diet Pepsi Caffeine Free" />
-            <p className="mt-2 font-bold">Diet Pepsi Caffeine Free</p>
-          </div>
-          <div className="text-center">
-            <img src="https://www.pepsi.com/en-us/refresh082123/images/cans/46948-titan-prs-can-12-oz-d-1-fr.png" alt="Pepsi Real Sugar" />
-            <p className="mt-2 font-bold">Pepsi Real Sugar</p>
+          <div className="hover:bg-gray-200 rounded-lg overflow-hidden shadow-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 h-[19vw]">
+            <p className="mt-2 text-center font-bold">Pepsi Zero Sugar Wild Cherry</p>
+            <div className="relative overflow-hidden h-[23vw] top-[11%] left-[24%]">
+              <img
+                src="https://www.pepsi.com/en-us/refresh082123/images/cans/44343_EC_Front.png"
+                alt="Pepsi Zero Sugar Wild Cherry"
+                className="absolute inset-0 h-full object-cover w-[50%]"
+              />
+            </div>
           </div>
         </div>
       </div>
